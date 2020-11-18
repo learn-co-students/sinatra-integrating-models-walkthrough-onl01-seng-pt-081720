4816d33f5697a3eec588d49aeb35a2e1950de04b
HaidyF <69495135+HaidyF@users.noreply.github.com>
2020-11-17 21:22:54 -0500

    Done.

--- app.rb
+++ app.rb
@@ -5,12 +5,10 @@ class App < Sinatra::Base
   get '/' do
     erb :index
   end
-
+  
   post '/' do
     text_from_user = params[:user_text]
-   
     @analyzed_text = TextAnalyzer.new(text_from_user)
-   
     erb :results
   end
 end
